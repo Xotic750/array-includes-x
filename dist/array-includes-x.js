69ee3819fb102ae5649f42d1e53cf5af7bc2c4ea
Graham Fairweather <xotic750@gmail.com>
2019-07-18 02:15:35 +0200

    :bookmark: v3.0.6
    Dependency updates

--- dist/array-includes-x.js
+++ dist/array-includes-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-17T19:27:13.976Z",
+  "date": "2019-07-18T00:15:29.717Z",
   "describe": "",
   "description": "Determines whether an array includes a certain element.",
   "file": "array-includes-x.js",
   "hash": "763a1eb91314968e84c9",
   "license": "MIT",
-  "version": "3.0.5"
+  "version": "3.0.6"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
